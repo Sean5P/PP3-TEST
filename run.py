231038d2210b15abb109d1ef55918e6e9f22cb11
Sean Kirwan <Sean@Kirwan.ie>
2024-01-09 02:23:31 +0000

    Update run.py
    Setup Data Model

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+class SurveyData:
+  def __init__(self, data):
+    self.data = data
+
+  def import_data(self, file_path):
+      pass
+
+  def parse_data(self):
+      pass
+  
+
+
+
+
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
