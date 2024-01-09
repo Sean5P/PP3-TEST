e4c070791b40f563bd792cb55a63d138f6fd6597
Sean Kirwan <Sean@Kirwan.ie>
2024-01-09 02:30:07 +0000

    Update run.py
    Add Import Data Method

--- run.py
+++ run.py
@@ -7,8 +7,14 @@ class SurveyData:
 
   def parse_data(self):
       pass
-  
 
+  import csv
+
+  def import_data(self, file_path):
+    with open(file_path, 'r') as file:
+      reader = csv.reader(file)
+      self.data = [row for row in reader]
+      
 
 
 
